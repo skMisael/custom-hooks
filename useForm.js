721db3e9906ecba8b98110250c481c53c9c326dd
web developer <m.pereyra@galloreyna.com>
2022-07-28 12:22:05 -0500

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+
+export const useForm = (initialForm = {}) => {
+
+        const [formState, setFormState] = useState(initialForm)
+        
+        const onInputChange =({target}) => {
+            const {name, value} = target;        
+            // console.log(e.target.name);
+            // console.log(e.target.value);
+            setFormState({
+                ...formState,
+                [ name ]: value
+            });
+        }
+
+        const onResetForm = () =>{
+            setFormState(initialForm);
+        }
+
+
+        return{
+            ... formState,
+            formState,
+            onInputChange,
+            onResetForm
+        }
+}
---
+++ useTodo.js
@@ -0,0 +1,108 @@
+import { useEffect, useReducer,useState } from "react"
+import { todoReducer } from "../08-reducer/todoReducer";
+
+
+const initialState = [];
+
+const init = () => {
+  return JSON.parse( localStorage.getItem('todos') ) || [];
+}
+
+
+export const useTodo = () => {
+
+        const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+        const [pag, setPag] = useState({ini:0,fin:5});
+
+        useEffect(() => {
+            const strn = JSON.stringify(todos);
+            localStorage.setItem('todos',strn);   
+                  
+        }, [todos])
+
+        useEffect(() => {
+            filterData();
+        }, []);
+        
+
+        const onNewTodo = (todo) => {
+            const action ={
+                type: 'Add Todo',
+                payload: todo
+            }
+            dispatchTodo(action);
+        }
+
+        const onDeleteTodo = (id) => {
+            const action ={
+                type: 'Remove Todo',
+                payload: id,
+            }
+            dispatchTodo(action);
+        }
+        
+        const onToggleTodo =(id) => {
+            const action ={
+            type: 'Toggle Todo',
+            payload: id,
+            }
+            dispatchTodo(action);
+        }
+
+
+        const next = () => {
+            if((todos.length)<=pag.fin)return;
+            const ini = pag.fin;
+            const fin = pag.fin+5;
+            setPag({ini,fin});
+        }
+
+        const back = () => {
+            if(pag.ini==0)return;
+            const fin = pag.fin-5;
+            const ini = (fin-5);            
+            setPag({ini,fin});
+        }
+        const onepag = (initial)=>{
+            const ini = (initial);  
+            const fin = ini+5;          
+            setPag({ini,fin});
+        }
+
+        const  filterData = () =>{
+            
+            todos.sort(function(a,b){
+                if (a.description.toLowerCase() <b.description.toLowerCase() ) {
+                return -1;
+                }
+                if (a.description.toLowerCase() >b.description.toLowerCase() ) {
+                return 1;
+                }
+                return 0;
+            }); 
+            // console.log(todos);
+            const arraytodos=todos.slice(pag.ini,pag.fin);
+             
+            return arraytodos;       
+        }
+      
+
+        
+        
+
+      
+
+    return {
+        todos,
+        onNewTodo,
+        onDeleteTodo,
+        onToggleTodo,
+        todosCount:todos.length,
+        filterData,
+        next,
+        back,
+        onepag,
+        pendingTodosCount:todos.filter( todo => !todo.done).length,
+    };
+}
